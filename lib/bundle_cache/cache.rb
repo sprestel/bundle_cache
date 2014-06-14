f8a7f76bc4f69313e10c5259ce060ff161bef37b
Sebastian Prestel <sebastian.prestel@yahoo.de>
2014-06-14 22:37:22 +0200

    dropbox-sdk
    * version bumped to 0.3.0

--- lib/bundle_cache/cache.rb
+++ lib/bundle_cache/cache.rb
@@ -1,20 +1,11 @@
 require "digest"
-require "aws-sdk"
+require "dropbox_sdk"
 
 module BundleCache
   def self.cache
-    # Setup AWS credentials
-    AWS.config({
-      :access_key_id => ENV["AWS_S3_KEY"],
-      :secret_access_key => ENV["AWS_S3_SECRET"],
-      :region => ENV["AWS_S3_REGION"] || "us-east-1"
-    })
-
-    acl_to_use = ENV["KEEP_BUNDLE_PRIVATE"] ? :private : :public_read
     bundle_dir = ENV["BUNDLE_DIR"] || "~/.bundle"
     processing_dir = ENV["PROCESS_DIR"] || ENV["HOME"]
 
-    bucket_name     = ENV["AWS_S3_BUCKET"]
     architecture    = `uname -m`.strip
 
     file_name       = "#{ENV['BUNDLE_ARCHIVE']}-#{architecture}.tgz"
@@ -27,7 +18,7 @@ module BundleCache
     bundle_digest = Digest::SHA2.file(lock_file).hexdigest
     old_digest    = File.exists?(old_digest) ? File.read(old_digest) : ""
 
-    if bundle_digest == old_digest
+    if bundle_digest.to_s.strip == old_digest.to_s.strip
       puts "=> There were no changes, doing nothing"
     else
       if old_digest == ""
@@ -39,38 +30,21 @@ module BundleCache
       end
 
       puts "=> Preparing bundle archive"
-      `tar -C #{File.dirname(bundle_dir)} -cjf #{file_path} #{File.basename(bundle_dir)} && split -b 5m -a 3 #{file_path} #{file_path}.`
-
+      `tar -C #{File.dirname(bundle_dir)} -cjf #{file_path} #{File.basename(bundle_dir)}`
+    
       if 1 == $?.exitstatus
         puts "=> Archive failed. Please make sure '--path=#{bundle_dir}' is added to bundle_args."
         exit 1
       end
 
+      client = DropboxClient.new(ENV["DROPBOX_ACCESS_TOKEN"])
 
-      parts_pattern = File.expand_path(File.join(processing_dir, "#{file_name}.*"))
-      parts = Dir.glob(parts_pattern).sort
-
-      s3 = AWS::S3.new
-      bucket = s3.buckets[bucket_name]
-
-      puts "=> Uploading the bundle"
-      gem_archive = bucket.objects[file_name]
-
-      puts "  => Uploading #{parts.length} parts"
-      gem_archive.multipart_upload(:acl => acl_to_use) do |upload|
-        parts.each_with_index do |part, index|
-          puts "    => Uploading #{part}"
-          File.open part do |part_file|
-            upload.add_part(part_file)
-            puts "      => Uploaded"
-          end
-        end
+      puts "=> Uploading"
+      begin
+        client.put_file(file_name, open(file_path, "r").read, true)
+        client.put_file(digest_filename, bundle_digest, true)
       end
-      puts "  => Completed multipart upload"
 
-      puts "=> Uploading the digest file"
-      hash_object = bucket.objects[digest_filename]
-      hash_object.write(bundle_digest, :acl => acl_to_use, :content_type => "text/plain")
     end
 
     puts "All done now."
--- lib/bundle_cache/install.rb
+++ lib/bundle_cache/install.rb
@@ -1,47 +1,35 @@
 require "digest"
-require "aws-sdk"
+require "dropbox_sdk"
 
 module BundleCache
   def self.install
     architecture = `uname -m`.strip
     file_name = "#{ENV['BUNDLE_ARCHIVE']}-#{architecture}.tgz"
     digest_filename = "#{file_name}.sha2"
-    bucket_name = ENV["AWS_S3_BUCKET"]
+
     bundle_dir = ENV["BUNDLE_DIR"] || "~/.bundle"
     processing_dir = ENV['PROCESS_DIR'] || ENV['HOME']
-
-    AWS.config({
-      :access_key_id => ENV["AWS_S3_KEY"],
-      :secret_access_key => ENV["AWS_S3_SECRET"],
-      :region => ENV["AWS_S3_REGION"] || "us-east-1"
-    })
-    s3 = AWS::S3.new
-    bucket = s3.buckets[bucket_name]
-
-    gem_archive = bucket.objects[file_name]
-    hash_object = bucket.objects[digest_filename]
+    
+    client = DropboxClient.new(ENV["DROPBOX_ACCESS_TOKEN"])
 
     puts "=> Downloading the bundle"
-    File.open("#{processing_dir}/remote_#{file_name}", 'wb') do |file|
-      gem_archive.read do |chunk|
-        file.write(chunk)
-      end
-    end
+    
+    contents, metadata = client.get_file_and_metadata(file_name)
+    open("#{processing_dir}/remote_#{file_name}", 'wb') {|f| f.puts contents }
+
     puts "  => Completed bundle download"
 
     puts "=> Extract the bundle"
     `cd #{File.dirname(bundle_dir)} && tar -xf "#{processing_dir}/remote_#{file_name}"`
 
     puts "=> Downloading the digest file"
-    File.open("#{processing_dir}/remote_#{file_name}.sha2", 'wb') do |file|
-      hash_object.read do |chunk|
-        file.write(chunk)
-      end
-    end
+    contents, metadata = client.get_file_and_metadata(digest_filename)
+    open("#{processing_dir}/remote_#{file_name}.sha2", 'wb') {|f| f.puts contents }
+    
     puts "  => Completed digest download"
 
     puts "=> All done!"
-  rescue AWS::S3::Errors::NoSuchKey
+  rescue => e
     puts "There's no such archive!"
   end
 
